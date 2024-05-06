b5f9acb2a7317a49dee6a705bd3ab8b0d0b95511
qasmark <99826972+qasmark@users.noreply.github.com>
2024-05-06 03:58:52 +0300

    fixed mouse event and mouse handler

--- lab5/task2/task2.py
+++ lab5/task2/task2.py
@@ -1,16 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QOpenGLWidget
-from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QOpenGLWidget, QLabel
+from PyQt5.QtCore import QTimer, pyqtSignal, Qt
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from PIL import Image
-
+import random
 
 class GameWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.openglWidget = None
+        self.rotation_label = None
         self.setWindowTitle("Memory Trainer 3D")
         self.setGeometry(100, 100, 800, 600)
 
@@ -19,6 +21,15 @@ class GameWindow(QMainWindow):
     def init_ui(self):
         self.openglWidget = MyOpenGLWidget(self)
         self.setCentralWidget(self.openglWidget)
+
+        self.rotation_label = QLabel(self)
+        self.rotation_label.setGeometry(10, 30, 200, 40)
+        self.rotation_label.setStyleSheet("background-color: rgba(255, 255, 255, 0.5);"
+                                          "border: 2px solid black;"
+                                          "color: black;")
+        self.rotation_label.setFont(QFont("Arial", 16))
+        self.rotation_label.setAlignment(Qt.AlignCenter)
+        self.rotation_label.setText("Rotations: 0")
 
         menubar = self.menuBar()
         game_menu = menubar.addMenu('Game Mode')
@@ -32,15 +43,21 @@ class GameWindow(QMainWindow):
         hard_action = game_menu.addAction('Hard (8x7)')
         hard_action.triggered.connect(lambda: self.openglWidget.set_game_mode(8, 7))
 
+        self.openglWidget.rotation_count_changed.connect(self.update_rotation_label)
+
+    def update_rotation_label(self, count):
+        self.rotation_label.setText(f"Rotations: {count}")
+
 
 class MyOpenGLWidget(QOpenGLWidget):
+    rotation_count_changed = pyqtSignal(int)
+
     def __init__(self, parent):
         super().__init__(parent)
         self.grid_width = 4
         self.grid_height = 3
         self.texture_id = None
         self.tile_states = [[False for _ in range(self.grid_width)] for _ in range(self.grid_height)]
-        self.click_count = 0
         self.clicked_tile = None
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.close_tiles)
@@ -51,12 +68,32 @@ class MyOpenGLWidget(QOpenGLWidget):
         self.animation_frame_interval = 16  # Frame moment
         self.animation_frame_count = int(self.animation_duration / self.animation_frame_interval)
         self.current_animation_frame = 0
+        self.tile_textures = {}
+        self.tile_numbers = {}
+        self.rotation_counts = {
+            (4, 3): 0,
+            (6, 5): 0,
+            (8, 7): 0
+        }
+
+    def load_all_tile_textures(self):
+        self.tile_textures = {}
+
+        for i in range(1, 17):
+            texture_path = f"Textures/tile_{i}.png"
+            texture_id = self.load_texture(texture_path)
+            if texture_id is not None:
+                self.tile_textures[f"tile_{i}"] = texture_id
+                self.tile_numbers[(i // 2, (i - 1) % 2)] = i
+            else:
+                print(f"Не удалось загрузить текстуру: {texture_path}")
 
     def initializeGL(self):
         glClearColor(0.2, 0.2, 0.2, 1.0)
         glEnable(GL_DEPTH_TEST)
 
         self.texture_id = self.load_texture("Textures/back.png")
+        self.load_all_tile_textures()
 
     def resizeGL(self, width, height):
         glViewport(0, 0, width, height)
@@ -87,24 +124,37 @@ class MyOpenGLWidget(QOpenGLWidget):
         start_pos_y = -((tile_size + gap) * grid_height) / 2.0
 
         glEnable(GL_TEXTURE_2D)
-        glBindTexture(GL_TEXTURE_2D, self.texture_id)
 
         for i in range(grid_height):
             for j in range(grid_width):
                 glPushMatrix()
                 if self.tile_states[i][j]:
-                    # If card is open and animation is active, rotate it
+                    if (i, j) in self.tile_textures:
+                        glBindTexture(GL_TEXTURE_2D, self.tile_textures[(i, j)])
+                    else:
+                        glBindTexture(GL_TEXTURE_2D, self.texture_id)
+
                     if self.clicked_tile == (i, j):
-                        glTranslatef(start_pos_x + j * (tile_size + gap) + tile_size / 2, start_pos_y + i * (tile_size + gap) + tile_size / 2, 0)
+                        # If tile open then use texture tile (doesn't work)
+                        # TODO: fix
+                        glBindTexture(GL_TEXTURE_2D, self.tile_textures[f"tile_{self.tile_numbers[(i, j)]}"])
+
+                        glTranslatef(start_pos_x + j * (tile_size + gap) + tile_size / 2,
+                                     start_pos_y + i * (tile_size + gap) + tile_size / 2, 0)
                         glRotatef(self.animation_angle, 0, 1, 0)
-                        glTranslatef(-start_pos_x - j * (tile_size + gap) - tile_size / 2, -start_pos_y - i * (tile_size + gap) - tile_size / 2, 0)
+                        glTranslatef(-start_pos_x - j * (tile_size + gap) - tile_size / 2,
+                                     -start_pos_y - i * (tile_size + gap) - tile_size / 2, 0)
+                else:
+                    glBindTexture(GL_TEXTURE_2D, self.texture_id)
+
                 glBegin(GL_QUADS)
                 glTexCoord2f(0.0, 0.0)
                 glVertex3f(start_pos_x + j * (tile_size + gap), start_pos_y + i * (tile_size + gap), 0.0)
                 glTexCoord2f(1.0, 0.0)
                 glVertex3f(start_pos_x + (j + 1) * tile_size + j * gap, start_pos_y + i * (tile_size + gap), 0.0)
                 glTexCoord2f(1.0, 1.0)
-                glVertex3f(start_pos_x + (j + 1) * tile_size + j * gap, start_pos_y + (i + 1) * tile_size + i * gap, 0.0)
+                glVertex3f(start_pos_x + (j + 1) * tile_size + j * gap, start_pos_y + (i + 1) * tile_size + i * gap,
+                           0.0)
                 glTexCoord2f(0.0, 1.0)
                 glVertex3f(start_pos_x + j * (tile_size + gap), start_pos_y + (i + 1) * tile_size + i * gap, 0.0)
                 glEnd()
@@ -112,11 +162,23 @@ class MyOpenGLWidget(QOpenGLWidget):
 
         glDisable(GL_TEXTURE_2D)
 
+    def generate_image_deck(self, size):
+        num_tiles = size[0] * size[1]
+        num_pairs = num_tiles // 2
+
+        all_images = [f'Textures/tile_{i}.png' for i in range(1, num_pairs + 1)]
+        image_deck = all_images * 2
+
+        random.shuffle(image_deck)
+
+        return image_deck
+
     def set_game_mode(self, width, height):
         self.grid_width = width
         self.grid_height = height
         self.tile_states = [[False for _ in range(self.grid_width)] for _ in range(self.grid_height)]
-        self.click_count = 0
+        self.rotation_counts[(self.grid_width, self.grid_height)] = 0
+        self.rotation_count_changed.emit(self.rotation_counts[(self.grid_width, self.grid_height)])
         self.clicked_tile = None
         self.timer.stop()
         self.animation_timer.stop()
@@ -136,55 +198,72 @@ class MyOpenGLWidget(QOpenGLWidget):
         x = event.x()
         y = event.y()
 
-        # Преобразуем координаты окна в OpenGL координаты
         viewport = glGetIntegerv(GL_VIEWPORT)
         win_x = x
         win_y = viewport[3] - y
         z = glReadPixels(win_x, win_y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT)
 
-        # Преобразуем координаты в мировые координаты
         win_x = x
         win_y = viewport[3] - y
         model_view = glGetDoublev(GL_MODELVIEW_MATRIX)
         projection_view = glGetDoublev(GL_PROJECTION_MATRIX)
         obj_x, obj_y, obj_z = gluUnProject(win_x, win_y, z[0][0], model_view, projection_view, viewport)
 
-        tile_size = 1.0
+        tile_size = 0.13
         gap = 0.1
 
         start_pos_x = -((tile_size + gap) * self.grid_width) / 2.0
         start_pos_y = -((tile_size + gap) * self.grid_height) / 2.0
 
-        # Определяем, на какую карточку было нажато
         i = int((obj_y - start_pos_y) / (tile_size + gap))
         j = int((obj_x - start_pos_x) / (tile_size + gap))
 
         if 0 <= i < self.grid_height and 0 <= j < self.grid_width:
             if not self.tile_states[i][j]:
-                # Если карточка не открыта, открываем ее и запускаем анимацию
+                # Если плитка не открыта, открываем ее
+                # TODO: fix cause it does'nt show when it has status open
+                self.rotation_counts[(self.grid_width, self.grid_height)] += 1
+                self.rotation_count_changed.emit(self.rotation_counts[(self.grid_width, self.grid_height)])
                 self.tile_states[i][j] = True
                 self.clicked_tile = (i, j)
                 self.animation_angle = 0
                 self.animation_timer.start(self.animation_frame_interval)
                 self.update()
+            else:
+                # Если плитка уже открыта, закрываем ее
+                # TODO: fix
+                self.tile_states[i][j] = False
+                self.clicked_tile = None
+                self.current_animation_frame = 0
+                self.update()
+
+    def open_tile(self, i, j):
+        # Открывает плитку с координатами (i, j) и устанавливает соответствующую текстуру
+        # TODO: fix
+        self.rotation_counts[(self.grid_width, self.grid_height)] += 1
+        self.rotation_count_changed.emit(self.rotation_counts[(self.grid_width, self.grid_height)])
+        self.tile_states[i][j] = True
+        self.clicked_tile = (i, j)
+        self.animation_angle = 0
+        self.animation_timer.start(self.animation_frame_interval)
+        self.update()
 
     def animate_tile_rotation(self):
-        # Обновляем угол поворота и перерисовываем сцену
         self.animation_angle += 180 / self.animation_frame_count
         self.current_animation_frame += 1
         self.update()
 
-        # По завершении анимации останавливаем таймер и сбрасываем состояние карточки
         if self.current_animation_frame >= self.animation_frame_count:
             self.animation_timer.stop()
             self.close_tiles()
 
     def close_tiles(self):
-        # Закрываем все открытые карточки
-        self.tile_states[self.clicked_tile[0]][self.clicked_tile[1]] = False
-        self.clicked_tile = None
-        self.current_animation_frame = 0
-        self.update()
+        if self.clicked_tile:
+            self.tile_states[self.clicked_tile[0]][self.clicked_tile[1]] = False
+            self.clicked_tile = None
+            self.current_animation_frame = 0
+            self.update()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
